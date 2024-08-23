13a4329ce4f69c2616644bbaa14e7bf65bb40765
KieVitz <s217442@sggw.edu.pl>
2024-08-23 19:22:46 +0200

    Add lombok

--- src/main/java/com/stahu/lox/model/Token.java
+++ src/main/java/com/stahu/lox/model/Token.java
@@ -1,17 +1,13 @@
 package com.stahu.lox.model;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Token {
     final TokenType type;
     final String lexeme;
     final Object literal;
     final int line;
-
-    public Token(TokenType type, String lexeme, Object literal, int line) {
-        this.type = type;
-        this.lexeme = lexeme;
-        this.literal = literal;
-        this.line = line;
-    }
 
     public String toString() {
         return type + " " + lexeme + " " + literal;
